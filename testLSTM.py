c4e2c10eea60e68ab276b165e5f6a025164a638a
Basasuya <961256668@qq.com>
2018-06-30 13:33:12 +0800

    new version

--- testLSTM.py
+++ testLSTM.py
@@ -1,11 +1,11 @@
 # LSTM for international airline passengers problem with regression framing
 import numpy
 import sys
-import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import read_csv
 import math
 from keras.models import Sequential
+from keras.models import load_model
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
@@ -24,7 +24,7 @@ testPos = []
 
 def split_dataset(dataPath, look_back=10):
     global Dataset, DataSplit
-    dataframe = pd.read_csv(dataPath, header=None)
+    dataframe = read_csv(dataPath, usecols=[1], engine='python', skipfooter=3)
     dataset = dataframe.values
     dataset = dataset.astype('float32')
     for i in dataset:
@@ -33,67 +33,34 @@ def split_dataset(dataPath, look_back=10):
 
 if __name__ == '__main__':
 	look_back = 10
-	doTimes = int(sys.argv[1])
 	for i in range(2, len(sys.argv)):
 		split_dataset('srcData/' + sys.argv[i], look_back)
 	scaler = MinMaxScaler(feature_range=(0, 1))
 	Dataset = scaler.fit_transform(Dataset)
+	model = load_model(sys.argv[1])
+
+
 	prePos = 0
 	for i in range(len(DataSplit)):
-		cnt = 0
-		stopEdge = int(DataSplit[i]*0.68)
 		for j in range(look_back+prePos, DataSplit[i]+prePos):
-			cnt = cnt + 1
-			if cnt < stopEdge:
-				trainX.append(Dataset[(j-look_back):j, 0])
-				trainY.append(Dataset[j, 0])
-				trainPos.append(j)
-			else:
-				testX.append(Dataset[(j-look_back):j, 0])
-				testY.append(Dataset[j, 0])
-				testPos.append(j)
+			trainX.append(Dataset[(j-look_back):j, 0])
+			trainPos.append(j)
 		prePos = prePos + DataSplit[i]
 
-	print(len(trainX))
 	trainX = numpy.array(trainX)
-	trainY = numpy.array(trainY)
-	testX = numpy.array(testX)
-	testY = numpy.array(testY)
 	trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-	testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-	# create and fit the LSTM network
-	model = Sequential()
-	model.add(LSTM(50, input_shape=(1, look_back)))
-	model.add(Dense(25))
-	model.add(Dense(1))
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	model.fit(trainX, trainY, epochs=doTimes, batch_size=1, verbose=2)
-	model.save("A1.h5")
-	# make predictions
 	trainPredict = model.predict(trainX)
-	testPredict = model.predict(testX)
 	# invert predictions
-	trainPredict = scaler.inverse_transform(trainPredict)
-	trainY = scaler.inverse_transform([trainY])
-	testPredict = scaler.inverse_transform(testPredict)
-	testY = scaler.inverse_transform([testY])
-
+	trainPredict = scaler.inverse_transform(trainPredict)	
 	trainPredictPlot = numpy.empty_like(Dataset)
 	trainPredictPlot[:, :] = numpy.nan
-	testPredictPlot = numpy.empty_like(Dataset)
-	testPredictPlot[:, :] = numpy.nan
 
 	cnt = 0
 	for i in trainPos:
 		trainPredictPlot[i,:] = trainPredict[cnt]
 		cnt = cnt + 1
 
-	cnt = 0
-	for i in testPos:
-		testPredictPlot[i,:] = testPredict[cnt]
-		cnt = cnt + 1
 
 	plt.plot(scaler.inverse_transform(Dataset))
 	plt.plot(trainPredictPlot)
-	plt.plot(testPredictPlot)
 	plt.show()
--- writeData.cpp
+++ writeData.cpp
@@ -4,37 +4,82 @@
 #include <cmath>
 using namespace std;
 
-
-int main() {
-    freopen("Basasuya.csv", "w", stdout);
-
+void trainSin() {
+    freopen("srcData/sin1.csv", "w", stdout);
     printf("Data Prediction, VIS in LSTM\n");
-
-    for(double i = 0; i < 10; i += 0.1) {
+    for(double i = 0; i < 20; i += 0.1) {
         printf("1,%.3f\n", sin(i));
     }
-
     printf("\nYes I Can\n");
 
 
-    freopen("Basasuya2.csv", "w", stdout);
-
+    freopen("srcData/sin2.csv", "w", stdout);
     printf("Data Prediction, VIS in LSTM\n");
-
-    for(double i = 0; i < 10; i += 0.1) {
-        printf("1,%.3f\n", i);
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", sin(i/2) + 10);
     }
-
     printf("\nYes I Can\n");
-
-    freopen("Basasuya3.csv", "w", stdout);
-
+    
+    freopen("srcData/sin3.csv", "w", stdout);
     printf("Data Prediction, VIS in LSTM\n");
-
-    for(double i = 10; i > 0; i -= 0.1) {
-        printf("1,%.3f\n", i);
+    for(double i = 20; i > 0; i -= 0.1) {
+        printf("1,%.3f\n", sin(i*2) + 20);
     }
-
     printf("\nYes I Can\n");
+
+    freopen("srcData/sin4.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", sin(i)/2 + 5);
+    }
+    printf("\nYes I Can\n");
+
+     freopen("srcData/sin5.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", sin(i)*2 + 15);
+    }
+    printf("\nYes I Can\n");
+}
+void testSin() {
+    freopen("srcData/sin6.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", cos(i));
+    }
+    printf("\nYes I Can\n");
+
+
+    freopen("srcData/sin7.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", sin(i/2) + 20);
+    }
+    printf("\nYes I Can\n");
+    
+    freopen("srcData/sin8.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 20; i > 0; i -= 0.1) {
+        printf("1,%.3f\n", sin(i*3) + 20);
+    }
+    printf("\nYes I Can\n");
+
+    freopen("srcData/sin9.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", sin(i)/2 + 15);
+    }
+    printf("\nYes I Can\n");
+
+     freopen("srcData/sin10.csv", "w", stdout);
+    printf("Data Prediction, VIS in LSTM\n");
+    for(double i = 0; i < 20; i += 0.1) {
+        printf("1,%.3f\n", sin(i)*3 + 25);
+    }
+    printf("\nYes I Can\n");
+}
+int main() {
+    //trainSin();
+    testSin();
 }
 
